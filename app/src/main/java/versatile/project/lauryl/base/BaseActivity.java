f7ce233c211724b1d02f302659467fa1ef525463
aliajahar90 <aliajahar90@gmail.com>
2020-10-02 03:18:30 +0530

    Merge branch 'master' of https://dev.azure.com/versatilecommerce/Lauryl/_git/Lauryl%20-%20Android into develop Build issue due to duplicate code
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/versatile/project/lauryl/network/api/RetrofitObj.kt

--- app/src/main/java/versatile/project/lauryl/base/BaseActivity.java
+++
@@ -1,6 +0,0 @@
-package versatile.project.lauryl.base;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public abstract class BaseActivity<V extends BaseViewModel> extends AppCompatActivity {
-}
