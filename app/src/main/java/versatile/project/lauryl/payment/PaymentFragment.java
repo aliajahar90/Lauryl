2fe6f38296e462db7362a910743b405af80797de
aliajahar90 <aliajahar90@gmail.com>
2020-10-02 02:57:21 +0530

    Payment| Razor pay integration

--- app/src/main/java/versatile/project/lauryl/payment/PaymentFragment.java
+++ app/src/main/java/versatile/project/lauryl/payment/PaymentFragment.java
@@ -14,6 +14,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 
 import com.google.gson.Gson;
+import com.razorpay.Razorpay;
 
 import versatile.project.lauryl.R;
 import versatile.project.lauryl.base.BaseBinding;
@@ -22,23 +23,24 @@ import versatile.project.lauryl.payment.data.PaymentBaseShareData;
 import versatile.project.lauryl.payment.viewModel.PaymentViewModel;
 import versatile.project.lauryl.utils.AllConstants;
 
-public  class PaymentFragment  extends BaseBinding<PaymentViewModel, PaymentFragmentBinding> {
+public class PaymentFragment extends BaseBinding<PaymentViewModel, PaymentFragmentBinding> {
     private PaymentFragmentBinding paymentFragmentBinding;
     private PaymentViewModel paymentViewModel;
-    public static final int PaymentTypeUpi=1;
-    public static final int PaymentTypeCards=2;
-    public static final int PaymentTypeNetBanking=3;
+    public static final int PaymentTypeUpi = 1;
+    public static final int PaymentTypeCards = 2;
+    public static final int PaymentTypeNetBanking = 3;
     private static PaymentBaseShareData paymentBaseShareData;
-    public static PaymentFragment newInstance(int viewType){
-        PaymentFragment paymentFragment=new PaymentFragment();
+    private Razorpay razorpay;
+
+    public static PaymentFragment newInstance(int viewType) {
+        PaymentFragment paymentFragment = new PaymentFragment();
         try {
             paymentBaseShareData = new Gson().fromJson((String) paymentFragment.getArguments().get(AllConstants.PaymentData), PaymentBaseShareData.class);
+        } catch (Exception e) {
+            Log.d("Error", "Null Pointer");
         }
-        catch (Exception e){
-            Log.d("Error","Null Pointer");
-        }
-        if(paymentBaseShareData==null) {
-            paymentBaseShareData=new PaymentBaseShareData();
+        if (paymentBaseShareData == null) {
+            paymentBaseShareData = new PaymentBaseShareData();
         }
         paymentBaseShareData.setViewType(viewType);
         return paymentFragment;
@@ -50,28 +52,41 @@ public  class PaymentFragment  extends BaseBinding<PaymentViewModel, PaymentFrag
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        paymentFragmentBinding= DataBindingUtil.inflate(inflater,R.layout.payment_fragment,container,false);
+        paymentFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.payment_fragment, container, false);
         displayView(paymentBaseShareData.getViewType());
         paymentFragmentBinding.setViewmodel(paymentViewModel);
         return paymentFragmentBinding.getRoot();
     }
 
-    private void displayView(int type){
-        switch (type){
-            case PaymentTypeCards:paymentFragmentBinding.paymentCard.getRoot().setVisibility(View.VISIBLE);
+    private void displayView(int type) {
+        switch (type) {
+            case PaymentTypeCards:
+                paymentFragmentBinding.paymentCard.getRoot().setVisibility(View.VISIBLE);
                 paymentFragmentBinding.paymentNetBank.getRoot().setVisibility(View.GONE);
                 paymentFragmentBinding.paymentUPI.getRoot().setVisibility(View.GONE);
+                activateCardsPayment();
+                deActivateUpiPayment();
+                deActivateNetBankingPayment();
                 break;
-            case PaymentTypeNetBanking: paymentFragmentBinding.paymentNetBank.getRoot().setVisibility(View.VISIBLE);
+            case PaymentTypeNetBanking:
+                paymentFragmentBinding.paymentNetBank.getRoot().setVisibility(View.VISIBLE);
                 paymentFragmentBinding.paymentCard.getRoot().setVisibility(View.GONE);
                 paymentFragmentBinding.paymentUPI.getRoot().setVisibility(View.GONE);
+                activateNetBankingPayment();
+                deActivateCardsPayment();
+                deActivateUpiPayment();
                 break;
             case PaymentTypeUpi:
-            default:paymentFragmentBinding.paymentUPI.getRoot().setVisibility(View.VISIBLE);
+            default:
+                paymentFragmentBinding.paymentUPI.getRoot().setVisibility(View.VISIBLE);
                 paymentFragmentBinding.paymentNetBank.getRoot().setVisibility(View.GONE);
                 paymentFragmentBinding.paymentCard.getRoot().setVisibility(View.GONE);
+                activateUpiPayment();
+                deActivateCardsPayment();
+                deActivateNetBankingPayment();
         }
     }
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -79,7 +94,8 @@ public  class PaymentFragment  extends BaseBinding<PaymentViewModel, PaymentFrag
 
 
     }
-    void onClicks(){
+
+    void onClicks() {
         paymentFragmentBinding.rlUpi.setOnClickListener(view -> {
             displayView(PaymentFragment.PaymentTypeUpi);
         });
@@ -90,8 +106,66 @@ public  class PaymentFragment  extends BaseBinding<PaymentViewModel, PaymentFrag
             displayView(PaymentFragment.PaymentTypeNetBanking);
         });
     }
+
     @Override
     protected void initializeViewModel() {
         paymentViewModel = new ViewModelProvider(this).get(PaymentViewModel.class);
+    }
+
+    void activateUpiPayment() {
+        paymentFragmentBinding.rlUpi.setBackground(getResources().getDrawable(R.drawable.selected_payment));
+        paymentFragmentBinding.imgUPI.setImageResource(R.drawable.upi_white);
+        paymentFragmentBinding.txtBankAccount.setTextColor(getResources().getColor(R.color.white));
+    }
+
+    void deActivateUpiPayment() {
+        paymentFragmentBinding.rlUpi.setBackgroundColor(getResources().getColor(R.color.white));
+        paymentFragmentBinding.imgUPI.setImageResource(R.drawable.upi);
+        paymentFragmentBinding.txtBankAccount.setTextColor(getResources().getColor(R.color.orange));
+    }
+
+    void activateCardsPayment() {
+        paymentFragmentBinding.rlCardsChild.setBackground(getResources().getDrawable(R.drawable.selected_payment));
+        paymentFragmentBinding.imgCards.setImageResource(R.drawable.cards_white);
+        paymentFragmentBinding.txtCards.setTextColor(getResources().getColor(R.color.white));
+
+    }
+
+    void deActivateCardsPayment() {
+        paymentFragmentBinding.rlCardsChild.setBackgroundColor(getResources().getColor(R.color.white));
+        paymentFragmentBinding.imgCards.setImageResource(R.drawable.cards);
+        paymentFragmentBinding.txtCards.setTextColor(getResources().getColor(R.color.orange));
+
+    }
+
+    void activateNetBankingPayment() {
+        paymentFragmentBinding.rlNetbank.setBackground(getResources().getDrawable(R.drawable.selected_payment));
+        paymentFragmentBinding.imgNetBank.setImageResource(R.drawable.netbanking_white);
+        paymentFragmentBinding.txtNetBanking.setTextColor(getResources().getColor(R.color.white));
+
+    }
+
+    void deActivateNetBankingPayment() {
+        paymentFragmentBinding.rlNetbank.setBackgroundColor(getResources().getColor(R.color.white));
+        paymentFragmentBinding.imgNetBank.setImageResource(R.drawable.netbanking);
+        paymentFragmentBinding.txtNetBanking.setTextColor(getResources().getColor(R.color.white));
+
+    }
+
+    private void initRazorpay() {
+
+        razorpay = new Razorpay(getActivity());
+        razorpay.getPaymentMethods(new Razorpay.PaymentMethodsCallback() {
+            @Override
+            public void onPaymentMethodsReceived(String result) {
+                Log.d("Result", "" + result);
+                //inflateLists(result);
+            }
+
+            @Override
+            public void onError(String error) {
+                Log.d("Get Payment error", error);
+            }
+        });
     }
 }
