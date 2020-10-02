e3ce82ec80c0d8ae310764bd6ff3648ee7862f47
aliajahar90 <aliajahar90@gmail.com>
2020-10-03 03:06:16 +0530

    Payment|Upi payment complete aar updated

--- app/src/main/java/versatile/project/lauryl/utils/AllConstants.java
+++ app/src/main/java/versatile/project/lauryl/utils/AllConstants.java
@@ -1,6 +1,30 @@
 package versatile.project.lauryl.utils;
 
 public class AllConstants {
-    //payment constants
-    public static final String PaymentData ="paymentdata";
+    public static class Payment {
+        //payment constants
+        public static final String PaymentData = "paymentdata";
+        public static final String CARD = "card";
+        public static final String DEBIT_CARD = "debit_card";
+        public static final String CREDIT_CARD = "credit_card";
+        public static final String NET_BANKING = "netbanking";
+        public static final String UPI = "upi";
+        public static final int PaymentViewOptions = 444;
+        public static final int PaymentViewCheckout = 555;
+
+
+        public static class HotBanks {
+            public static final String SBIN = "SBIN";
+            public static final String HDFC = "HDFC";
+            public static final String BARB_R = "BARB_R";
+            public static final String ICIC = "ICIC";
+            public static final String CNRB = "CNRB";
+            public static final String KKBK = "KKBK";
+        }
+
+        public static class Errors {
+            public static final String ERROR_VPA_VALIDATION = "Unable to validate vpa /n Please try Again";
+        }
+    }
+
 }
