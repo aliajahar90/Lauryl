6e21fb5169a82503aa6dfe0a999868805a6b9323
Kevin Occoner <pavan.arepalli@de-haze.com>
2020-10-16 18:14:15 +0530

    Map dynamic location fixed added.

--- app/src/main/java/versatile/project/lauryl/fragment/MapLocationFragment.kt
+++ app/src/main/java/versatile/project/lauryl/fragment/MapLocationFragment.kt
@@ -1,30 +1,121 @@
 package versatile.project.lauryl.fragment
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.synthetic.main.map_location_fragment.*
+import timber.log.Timber
 import versatile.project.lauryl.R
 import versatile.project.lauryl.screens.HomeScreen
 
-class MapLocationFragment: Fragment() {
+
+open class MapLocationFragment : Fragment(), OnMapReadyCallback, LocationListener {
+    private var googleMap: GoogleMap? = null
+    private val REQUEST_CODE = 101
+    private var locationManager: LocationManager? = null
+    private val MIN_TIME: Long = 400
+    private val MIN_DISTANCE = 1000f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.map_location_fragment,container,false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val inview = inflater.inflate(R.layout.map_location_fragment, container, false)
+        (childFragmentManager.findFragmentById(R.id.googleMap) as SupportMapFragment).getMapAsync(this)
+           return inview
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         confirm_location_btn.setOnClickListener {
             (activity as HomeScreen).displayChangeAddressFragment()
-
         }
     }
+
+    private fun fetchLocation() {
+        context?.let {
+            if (ActivityCompat.checkSelfPermission(
+                    it, Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    it, Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    it as Activity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE
+                )
+                return
+            }
+            googleMap?.isMyLocationEnabled = true
+            locationManager =context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            locationManager!!.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                fetchLocation()
+            }
+        }
+    }
+
+
+
+    override fun onMapReady(p0: GoogleMap?) {
+        googleMap = p0
+        googleMap?.mapType = GoogleMap.MAP_TYPE_TERRAIN
+        fetchLocation()
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        location.let {
+            val latLng = it?.latitude?.let { it1 -> location?.longitude?.let { it2 ->
+                LatLng(it1,
+                    it2
+                )
+            } }
+            val cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 15f)
+            googleMap?.animateCamera(cameraUpdate)
+            locationManager!!.removeUpdates(this)
+        }
+
+    }
+
+    override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+    }
+
+    override fun onProviderEnabled(p0: String?) {
+    }
+
+    override fun onProviderDisabled(p0: String?) {
+    }
+
 
 }
